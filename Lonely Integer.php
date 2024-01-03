891e0109ca93714dbac0da65c789e3f428d4b45b
Mizanur Rahman <mizanur.rahman@gmail.com>
2024-01-03 10:55:07 -0500

    Lonely Integer

---
+++ Lonely Integer.php
@@ -0,0 +1,15 @@
+// Lonely Integer
+// https://www.hackerrank.com/challenges/lonely-integer/problem
+
+<?php
+function lonelyinteger($a)
+{
+    return array_search(1, array_count_values($a));
+}
+
+$__fp = fopen("php://stdin", "r");
+fscanf($__fp, "%d", $_a_cnt);
+$_a = fgets($__fp);
+$_a = split(' ', $_a);
+$res = lonelyinteger($_a);
+echo $res;
