7ba0b48b112f49ad6e58390db59c0c297e876123
Mizanur Rahman <mizanur.rahman@gmail.com>
2024-01-04 13:37:17 -0500

    Project Euler #2: Even Fibonacci numbers

---
+++ Project Euler #2: Even Fibonacci numbers.php
@@ -0,0 +1,38 @@
+// Project Euler #2: Even Fibonacci numbers
+// https://www.hackerrank.com/contests/projecteuler/challenges/euler002/problem
+
+<?php
+$fibs = fibo(80);
+$handle = fopen("php://stdin", "r");
+$t = fgets($handle);
+
+for ($i = 0; $i < $t; $i++) {
+    $num = trim(fgets($handle));
+
+    $sum = 0;
+    foreach ($fibs as $fib) {
+        if ($fib <= $num) {
+            $sum += $fib;
+        }
+    }
+
+    print($sum) . "\n";
+}
+fclose($handle);
+
+
+function fibo($n)
+{
+    $prev = 1;
+    $next = 2;
+    $fibs = array(2);
+    for ($i = 1; $i < $n; $i++) {
+        $tmp = $next;
+        $next = $next + $prev;
+        $prev = $tmp;
+        if (($next + $prev) % 2 == 0)
+            $fibs[] = $next + $prev;
+    }
+
+    return $fibs;
+}
