6e3ef54b4c9142ae26dc6ae24c9f55d1bd239894
Mizanur Rahman <mizanur.rahman@gmail.com>
2024-01-05 13:22:32 -0500

    Project Euler #18: Maximum path sum I

---
+++ Project Euler #18: Maximum path sum I.php
@@ -0,0 +1,25 @@
+// Project Euler #18: Maximum path sum I
+// https://www.hackerrank.com/contests/projecteuler/challenges/euler018/problem
+
+<?php
+$handle = fopen ("php://stdin","r");
+$t = fgets($handle);
+
+for($x=0; $x<$t; $x++)
+{
+    $num = trim(fgets($handle));
+    $numbers = array();
+    for($j = 0;$j<$num;$j++)
+    {
+        $numbers[] = explode(" ",trim(fgets($handle)));
+    }
+    
+    $max = 0;
+    for ($i = $num - 2; $i >= 0; $i--) {
+        for ($j = 0; $j <= $i; $j++) {
+            $numbers[$i][$j] += max($numbers[$i+1][$j], $numbers[$i+1][$j+1]);
+        }        
+    }
+    print($numbers[0][0])."\n";
+}
+fclose($handle);
