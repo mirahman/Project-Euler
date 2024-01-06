0b3781ddd3f235c4c3737e31e9d0f3510500f1a8
Mizanur Rahman <mizanur.rahman@gmail.com>
2024-01-06 16:06:59 -0500

    Project Euler #34: Digit factorials

---
+++ Project Euler #34: Digit factorials.php
@@ -0,0 +1,26 @@
+// Project Euler #34: Digit factorials
+// https://www.hackerrank.com/contests/projecteuler/challenges/euler034/problem
+
+<?php
+$factorial = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880];
+$factorialSum = [];
+
+$handle = fopen("php://stdin", "r");
+$number = fgets($handle);
+$total = 0;
+
+for ($i = 10; $i <= $number; $i++) {
+    $sum = 0;
+    $n = $i;
+    while ($n != 0) {
+        $tmp = $n % 10;
+        $sum += $factorial[$tmp];
+        $n = intval($n / 10);
+    }
+
+    if ($sum % $i == 0) {
+        $total += $i;
+    }
+}
+
+echo $total . "\n";
